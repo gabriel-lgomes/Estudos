37c4be3cd40bee93a2de70933b8e26e793701453
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2022-04-13 20:38:53 -0300

    novos arquivos

---
+++ JAVASCRIPT ES6/objetos/prototype/scripts.js
@@ -0,0 +1,39 @@
+// Crie uma função construtora de Pessoas
+// Deve conter nome, sobrenome e idade
+// Crie um método no protótipo que retorne
+// o nome completo da pessoa
+
+function Pessoas (nome, sobrenome, idade) {
+	this.nome = nome;
+	this.sobrenome = sobrenome;
+	this.idade = idade;
+}  
+
+Pessoas.prototype.nomeCompleto = function () {
+	return this.nome + ' ' + this.sobrenome ;
+}
+
+const gabriel = new Pessoas ('Gabriel', 'Lopes', 25);
+
+// Liste os métodos acessados por 
+// dados criados com NodeList,
+// HTMLCollection, Document
+
+console.log(Object.getOwnPropertyNames(NodeList.prototype));
+console.log(Object.getOwnPropertyNames(HTMLCollection.prototype));
+console.log(Object.getOwnPropertyNames(Document.prototype));
+ 
+
+// Liste os construtores dos dados abaixo
+const li = document.querySelector('li');
+
+li; //HtmlLIElement
+li.click; // Função
+li.innerText; // String
+li.value; // Number
+li.hidden; // Boolean
+li.offsetLeft; // Number
+li.click(); // Undefined
+
+// Qual o construtor do dado abaixo:
+li.hidden.constructor.name; // String
