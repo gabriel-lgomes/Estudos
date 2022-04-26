c27c441cb5cba308bc34999e334018a2da36ad98
gabriel-lgomes <gabriel_lopes.g@hotmail.com>
2022-04-26 19:01:07 -0300

    Refatoração

--- JAVASCRIPT ES6/objetos/arrays/scripts.js
+++ JAVASCRIPT ES6/objetos/arrays/scripts.js
@@ -4,7 +4,7 @@ const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
 // Adicione 'Arroz' ao final da array
 // Adicione 'Peixe' e 'Batata' ao início da array
 
-const primeiraComida = comidas.shift();
+const primeiraComida = comidas.shift(); 
 const ultimaComida = comidas.pop();
 comidas.push('Arroz');
 comidas.unshift('Peixe', 'Batata');
@@ -14,11 +14,11 @@ const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
 // Inverta a ordem dos estudantes
 // Verifique se Joana faz parte dos estudantes
 // Verifique se Juliana faz parte dos estudantes
-
 estudantes.sort();
 estudantes.reverse();
-console.log(estudantes.includes('Joana'));
-console.log(estudantes.includes('Juliana')); 
+estudantes.includes('Joana');
+estudantes.includes('Juliana');
+
 
 let html = `<section>
               <div>Sobre</div>
@@ -28,11 +28,12 @@ let html = `<section>
 // Substitua section por ul e div com li,
 // utilizando split e join
 
-const newHtml = html.split('section').join('ul').split('div').join('li');
+const newHtml = html.split('section').join('ul').split('div').join('li');  
 
 const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
 // Remova o último carro, mas antes de remover
 // salve a array original em outra variável
 
-const carrosAntigos = carros.slice();
+const oldCars = carros.slice();
 carros.pop();
+ 
